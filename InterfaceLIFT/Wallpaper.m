51cfd80c542eea6614cdd213b72374bbb5affc2b
Matt Rajca <matt.rajca@me.com>
2012-09-08 19:29:45 -0500

    download url

--- InterfaceLIFT/Wallpaper.m
+++ InterfaceLIFT/Wallpaper.m
@@ -9,27 +9,7 @@
 
 @implementation Wallpaper
 
-#define MAGIC_DOWNLOAD_KEY @"7yz4ma1"
-
 @synthesize title, identifier, previewURL = _previewURL, thumbnail;
-
-- (NSURL *)downloadURL {
-	if (_previewURL == nil)
-		return nil;
-	
-	int screenWidth = (int) CGRectGetWidth([NSScreen mainScreen].frame);
-	int screenHeight = (int) CGRectGetHeight([NSScreen mainScreen].frame);
-	
-	NSString *downloadURLString = [[_previewURL absoluteString] stringByReplacingOccurrencesOfString:@"previews"
-																						  withString:MAGIC_DOWNLOAD_KEY];
-	
-	NSString *resolutionString = [NSString stringWithFormat:@"_%dx%d.jpg", screenWidth, screenHeight];
-	
-	downloadURLString = [downloadURLString stringByReplacingOccurrencesOfString:@".jpg"
-																	 withString:resolutionString];
-	
-	return [NSURL URLWithString:downloadURLString];
-}
 
 - (void)dealloc {
 	[title release];
